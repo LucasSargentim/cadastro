173f16e93b9913af7cbe020bd7330694d988bef9
LucasSargentim <lucas.sargentim@hotmail.com>
2016-06-27 09:23:34 -0300

    Create cadastro.php

---
+++ cadastro.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+<title>Cadastro</title>
+</head>
+
+<body>
+	<form name="signup" method="post" action="cadastrando.php"> 
+		Nome: <input type="text" name="nome" />
+		Sobrenome: <input type="text" name="sobrenome" />
+		Pais: <input type="text" name="pais" />
+		Estado: <input type="text" name="estado"/>
+		<input type="submit" value="Cadastrar" />
+	</form>
+</body>
+
+</html>
